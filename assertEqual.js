bb7a026175a46ba13f73c01ca5056527b7cab7a8
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-01 22:40:09 +0000

    used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, actual + " !== " + expected);
+  console.assert(actual === expected, `${actual}  !== ${expected}`);
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
