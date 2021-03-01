60af7aebb2140a86c628612d6ea0ec568a7bfe13
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-01 22:12:24 +0000

    created asset check, no emoji sadly

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, actual + " !== " + expected);
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
