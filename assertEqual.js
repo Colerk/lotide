8a6491455da4d8e711668e44bce48864aecc999a
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-02 00:16:20 +0000

    fixed to be a log instead of assert as instructed

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,7 @@
 const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, `${actual}  !== ${expected}`);
-  if (actual === expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual}  !== ${expected}`)
+  } else if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
 };
