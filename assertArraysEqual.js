56c057b959aed7ce0e56b01f80443429861f5978
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-02 23:29:32 +0000

    assertArraysEqual.js added

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = function(arr1, arr2) {
+  let current = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (current) {
+      if (arr1[i] !== arr2[i]) {
+        current = false;
+      }
+    }
+  }
+  if (current) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1}  !== ${arr2}`);
+  }
+};
