fc3eafa818e5bb6875aaa3dc32659e80d533f5fe
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-08 20:16:34 +0000

    testTail

--- tail.js
+++ tail.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, `${actual}  !== ${expected}`);
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3);
+module.exports = tail;
