2e956713d7eef1031fb1d2dda57f6a35b306b5bf
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-08 20:13:05 +0000

    created test files

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, `${actual}  !== ${expected}`);
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
